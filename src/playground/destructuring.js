19aadf27672e014a6b52756985af591e382c51be
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-07 12:56:34 -0800

    end of es6 destructuring basics

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,1 +1,29 @@
-console.log('destructuring');
+// const person = {
+//     name: 'Julien',
+//     age: 99,
+//     location: {
+//         city: 'Middle of Nowhere',
+//         temp: 92
+//     }
+// };
+
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`)
+
+// const { city, temp: temperature } = person.location; 
+// if (temperature && city ) {
+//     console.log(`It's ${temperature} in ${city}.`)
+// } 
+
+
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        name: 'Penguin'
+    }
+}
+
+const { name: publisherName = 'Self-Published'} = book.publisher;
+
+console.log(publisherName); // Peguin, Self-Published
