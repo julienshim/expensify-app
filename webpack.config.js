6fd3496dfa81b26784e4abcf6d2a61931356843c
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-08 15:45:53 -0800

    getting rid of the package json lock file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', // configuration needs to be explicitly state or a lot of performance warnings
-  entry: './src/playground/hoc.js', // entry -> output
+  entry: './src/app.js', // entry -> output
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
