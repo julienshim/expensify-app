b3c754943beac4a99715760a418a3a2acf2e8829
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-13 23:36:56 -0800

    submit form complete

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -57,7 +57,12 @@ export default class ExpenseForm extends React.Component {
       this.setState(() => ({
         error: ''
       }));
-      console.log('Submitted');
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
     }
   };
 
