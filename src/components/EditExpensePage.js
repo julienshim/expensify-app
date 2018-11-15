9d8a65e28f98886c2a67db9efaf3b4613e1bb83f
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-14 17:15:54 -0800

    reorder import action expenses components

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { removeExpense, editExpense } from '../actions/expenses';
+import { editExpense, removeExpense } from '../actions/expenses';
 
 const EditExpensePage = props => {
   console.log(props);
