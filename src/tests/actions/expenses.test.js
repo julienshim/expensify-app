bbb9c8a38fcb5e4ba4d1930b05c7f809c118af45
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-16 13:26:43 -0800

    expense action object with provided values assertion

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -9,12 +9,31 @@ test('should setup remove expense action object', () => {
 });
 
 test('should setup edit expense action object', () => {
-  const action = editExpense('123abc', { amount: '123' });
+  const action = editExpense('123abc', { amount: 123 });
   expect(action).toEqual({
     type: 'EDIT_EXPENSE',
     id: '123abc',
     updates: {
-      amount: '123'
+      amount: 123
     }
   });
 });
+
+test('should setup add expense action object with provided values', () => {
+  const expenseData = {
+    description: 'Rent',
+    amount: 109500,
+    createdAt: 1000,
+    note: "This was last month's rent"
+  };
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  });
+});
+
+test('should setup add expense action object with default values', () => {});
