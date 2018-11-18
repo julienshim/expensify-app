c760e5e97aa28f464e44c4fba5455b9259eb2b7b
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-17 23:40:15 -0800

    by end date assertion, by date assertion, by amount assertion

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -50,3 +50,36 @@ test("should filter by startDate", () => {
   const result = selectExpenses(expenses, filters);
   expect(result).toEqual([expenses[2], expenses[0]]);
 });
+
+test("should filter by endDate", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: moment(0)
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[0], expenses[1]]);
+});
+
+test("should filter by date", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
+
+test("should filter by amount", () => {
+  const filters = {
+    text: "",
+    sortBy: "amount",
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
+});
