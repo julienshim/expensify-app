f315042c97c4bcbc5779ae7dfb5db9d66e2ce23c
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-18 18:49:30 -0800

    add expense, edit expense (with id and update) and edit expense with invalid id)

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import expensesReducer from "../../reducers/expenses";
 import expenses from "../fixtures/expenses";
 
@@ -18,6 +19,66 @@ test("should remove expense by id", () => {
 test("should not remove expenses if id not found", () => {
   const action = {
     type: "REMOVE_EXPENSE",
+    id: "-1"
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+// should add expense
+
+test("should add expense", () => {
+  const action = {
+    type: "ADD_EXPENSE",
+    expense: {
+      id: "4",
+      description: "Hotdog Ryan",
+      note: "",
+      amount: 65000,
+      createdAt: moment(0)
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([
+    expenses[0],
+    expenses[1],
+    expenses[2],
+    {
+      id: "4",
+      description: "Hotdog Ryan",
+      note: "",
+      amount: 65000,
+      createdAt: moment(0)
+    }
+  ]);
+});
+
+// should edit expense
+
+test("should not remove expenses if id not found", () => {
+  const action = {
+    type: "EDIT_EXPENSE",
+    id: "1",
+    updates: {
+      amount: 2000000000
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([
+    {
+      ...expenses[0],
+      amount: 2000000000
+    },
+    expenses[1],
+    expenses[2]
+  ]);
+});
+
+// should not edit expense if expense not found
+
+test("should not remove expenses if id not found", () => {
+  const action = {
+    type: "EDIT_EXPENSE",
     id: "-1"
   };
   const state = expensesReducer(expenses, action);
