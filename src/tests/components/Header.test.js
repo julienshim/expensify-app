255a63c80b64bde1d4defd1d257d4b20491d6f59
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-19 00:49:34 -0800

    react-test-render/shallow, initial header snapshot testing

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ShallowRenderer from "react-test-renderer/shallow";
+import Header from "../../components/Header";
+// react-test-renderer
+
+test("should render Header correctly", () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<Header />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  console.log(renderer.getRenderOutput());
+});
