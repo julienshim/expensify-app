2c26e6e4e12c30b3d54aeb9663c65d33df5a0f25
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-23 21:11:12 -0800

    initial expenselistitem assertion

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 // Export a stateless functional component
 // descripton, amount, createdAt
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+export const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
