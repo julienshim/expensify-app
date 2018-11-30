7ce2aab7555d272128c1569e7fbd9a9feff0aa39
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-30 15:58:58 -0800

    assertion set note on textarea change

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -23,4 +23,22 @@ test('should render error for invalid form submission', ()=> {
   });
   expect(wrapper.state('error').length).toBeGreaterThan(0);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', ()=> {
+  const value = 'New description';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(0).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('description')).toBe(value);
+});
+
+test('should set note on textarea change', ()=> {
+  const value = 'New note';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('textarea').at(0).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('note')).toBe(value);
 })
