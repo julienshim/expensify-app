6e961c9ef5559f9f57989c0b0f293bbe2010b5f9
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-30 15:47:12 -0800

    expenseform should render error for invalid form submission

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,14 @@ test("should render ExpenseForm correctly with expense data", () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+
+test('should render error for invalid form submission', ()=> {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('form').simulate('submit', {
+      preventDefault:()=> {}
+  });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+})
