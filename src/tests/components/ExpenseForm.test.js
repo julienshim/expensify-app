ad6d61eec9a7db4302f526afaa30ea49c9b86887
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-30 16:29:40 -0800

    should not set amount if invalid, should set amount if valid input assertions

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -41,4 +41,27 @@ test('should set note on textarea change', ()=> {
     target: {value}
   });
   expect(wrapper.state('note')).toBe(value);
-})
+});
+
+// should set amount if valid input
+// 23.50
+test('should set amount if valid input', ()=> {
+  const value = "23.50";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('amount')).toBe(value);
+});
+
+//should not set amount if invalid put
+// 12.1222
+
+test('should not set amount if invalid input', ()=> {
+  const value = "12.122";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('amount')).toBe('');
+});
