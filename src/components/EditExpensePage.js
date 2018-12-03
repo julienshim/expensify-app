29f401beee4dcb03778abab7297e8471033e8e32
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-02 18:50:20 -0800

    editexpensepage test js assertions and converstion

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -4,39 +4,55 @@ import PropTypes from 'prop-types';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = ({ expense, dispatch, history }) => (
-  <div>
-    <ExpenseForm
-      expense={expense}
-      onSubmit={() => {
-        // Dispatch the action to edit the expense
-        dispatch(editExpense(expense.id, expense));
-        // Redirect to the dashboard
-        history.push('/');
-      }}
-    />
-    <button
-      type="button"
-      onClick={() => {
-        dispatch(removeExpense({ id: expense.id }));
-        history.push('/');
-      }}
-    >
-      Remove
-    </button>
-  </div>
-);
+export class EditExpensePage extends React.Component {
+  onSubmit = expense => {
+    // eslint-disable-next-line no-shadow
+    const { editExpense, history } = this.props;
+    editExpense(expense.id, expense);
+    history.push('/');
+  };
+
+  onRemove = () => {
+    // eslint-disable-next-line no-shadow
+    const { removeExpense, expense, history } = this.props;
+    removeExpense({ id: expense.id });
+    history.push('/');
+  };
+
+  render() {
+    const { expense } = this.props;
+    return (
+      <div>
+        <ExpenseForm expense={expense} onSubmit={this.onSubmit} />
+        <button type="button" onClick={this.onRemove}>
+          Remove
+        </button>
+      </div>
+    );
+  }
+}
 
 EditExpensePage.propTypes = {
+  removeExpense: PropTypes.func,
+  editExpense: PropTypes.func,
+  history: PropTypes.shape({
+    push: PropTypes.func
+  }),
   expense: PropTypes.shape({
     id: PropTypes.string
-  }),
-  history: PropTypes.func,
-  dispatch: PropTypes.func
+  })
 };
 
 const mapStateToProps = (state, props) => ({
   expense: state.expenses.find(expense => expense.id === props.match.params.id)
 });
 
-export default connect(mapStateToProps)(EditExpensePage);
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: data => dispatch(removeExpense(data))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditExpensePage);
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { shallow } from "enzyme";
+import React from 'react';
+import { shallow } from 'enzyme';
 // import toJson from "enzyme-to-json";
 // import ShallowRenderer from "react-test-renderer/shallow";
-import Header from "../../components/Header";
+import Header from '../../components/Header';
 // react-test-renderer
 
-test("should render Header correctly", () => {
+test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
 
