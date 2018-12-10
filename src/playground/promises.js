971fba555ae754ce547a369b1f8a8f989a75fad5
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-10 11:30:27 -0800

    promise chaining

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,7 +1,7 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve({ name: 'Julien', age: 35 });
-    reject(new Error('Something went wrong'));
+    resolve({ name: 'Julien', age: 35 });
+    // reject(new Error('Something went wrong'));
   }, 5000);
 });
 
@@ -12,6 +12,11 @@ promise
   .then(data => {
     // eslint-disable-next-line no-console
     console.log('1', data);
+    return 'some data';
+  })
+  .then(str => {
+    // eslint-disable-next-line no-console
+    console.log('does this run?', str);
   })
   .catch(error => {
     // eslint-disable-next-line no-console
