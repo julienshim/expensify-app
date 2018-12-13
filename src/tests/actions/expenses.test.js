73bf6f057d9f81f21289ea0f36854016e0999426
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-12 18:55:26 -0800

    cleaned up spacing

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -9,7 +9,6 @@ import {
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
-
 const createMockStore = configureMockStore([thunk]);
 
 test('should setup remove expense action object', () => {
@@ -62,21 +61,24 @@ test('should add expense to database and store', done => {
     createdAt: 1000
   };
 
-  store.dispatch(startAddExpense(expenseData)).then(() => {
-    const actions = store.getActions();
-    expect(actions[0]).toEqual({
-      type: 'ADD_EXPENSE',
-      expense: {
-        id: expect.any(String),
-        ...expenseData
-      }
-    });
+  store
+    .dispatch(startAddExpense(expenseData))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+          id: expect.any(String),
+          ...expenseData
+        }
+      });
 
-    return database.ref(`expenses/${actions[0].expense.id}`).once('value');
-  }).then((snapshot)=> {
-    expect(snapshot.val()).toEqual(expenseData);
-    done();
-  });
+      return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    })
+    .then(snapshot => {
+      expect(snapshot.val()).toEqual(expenseData);
+      done();
+    });
 });
 
 test('should add expense wtih defaults to database and store', () => {});
