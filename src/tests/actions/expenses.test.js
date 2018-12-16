673e89669966e1811a750753a77b54d8e7ed65b2
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-15 20:44:31 -0800

    shoudl set expense action object with data assertion, set_expenses action

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -4,12 +4,24 @@ import {
   startAddExpense,
   addExpense,
   editExpense,
-  removeExpense
+  removeExpense,
+  setExpenses
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach(done => {
+  const expenseData = {};
+  expenses.forEach(({ id, description, note, amount, createdAt }) => {
+    expenseData[id] = { description, note, amount, createdAt };
+  });
+  database
+    .ref('expenses')
+    .set(expenseData)
+    .then(() => done());
+});
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
@@ -37,20 +49,6 @@ test('should setup add expense action object with provided values', () => {
     expense: expenses[2]
   });
 });
-
-// test('should setup add expense action object with default values', () => {
-//   const action = addExpense();
-//   expect(action).toEqual({
-//     type: 'ADD_EXPENSE',
-//     expense: {
-//       id: expect.any(String),
-//       description: '',
-//       note: '',
-//       amount: 0,
-//       createdAt: 0
-//     }
-//   });
-// });
 
 test('should add expense to database and store', done => {
   const store = createMockStore({});
@@ -108,4 +106,12 @@ test('should add expense wtih defaults to database and store', done => {
       expect(snapshot.val()).toEqual(expenseDefaults);
       done();
     });
+});
+
+test('should setup set expense action object with data', () => {
+  const action = setExpenses(expenses);
+  expect(action).toEqual({
+    type: 'SET_EXPENSES',
+    expenses
+  });
 });
