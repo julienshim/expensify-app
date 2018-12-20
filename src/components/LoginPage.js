60fb8a5c5fea7aa495ba0d2822115408b3a03c04
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-19 19:09:21 -0800

    proptypes

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import startLogin from '../actions/auth';
 
-const LoginPage = ({ startLogin }) => (
-        <div>
-            <button type="button" onClick={startLogin}>Login</button>
-        </div>
+// eslint-disable-next-line no-shadow
+export const LoginPage = ({ startLogin }) => (
+  <div>
+    <button type="button" onClick={startLogin}>
+      Login
+    </button>
+  </div>
 );
 
-const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => dispatch(startLogin())
+LoginPage.propTypes = {
+  startLogin: PropTypes.func
+};
+
+const mapDispatchToProps = dispatch => ({
+  startLogin: () => dispatch(startLogin())
 });
 
-
-export default connect(undefined, mapDispatchToProps)(LoginPage);
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(LoginPage);
