2a6e4cadd507f6bfa91647aec83f0751033e8e38
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-20 20:13:58 -0800

    spacing

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,5 +3,4 @@ import { firebase, googleAuthProvider } from '../firebase/firebase';
 export const startLogin = () => () =>
   firebase.auth().signInWithPopup(googleAuthProvider);
 
-export const startLogout = () => () =>
-  firebase.auth().signOut();
+export const startLogout = () => () => firebase.auth().signOut();
