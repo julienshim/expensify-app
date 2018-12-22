494833430a765f49d8d809ea39b0c44d7eb91b53
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-21 21:38:20 -0800

    spacing issues and disable no-shadow eslint

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
-export const Header = ({startLogout}) => (
+// eslint-disable-next-line no-shadow
+export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
     <NavLink exact to="/" activeClassName="is-active">
@@ -18,13 +20,21 @@ export const Header = ({startLogout}) => (
     <NavLink to="/help" activeClassName="is-active">
       Help
     </NavLink>
-    <button type="button" onClick={startLogout}>Logout</button>
+    <button type="button" onClick={startLogout}>
+      Logout
+    </button>
   </header>
 );
 
+Header.propTypes = {
+  startLogout: PropTypes.func
+};
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   startLogout: () => dispatch(startLogout())
 });
 
-export default connect(undefined, mapDispatchToProps)(Header);
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(Header);
